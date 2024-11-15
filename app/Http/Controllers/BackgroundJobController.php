8266aacebea38cdcd3b6d0e6d11c12195e105891
Dalebza10111 <lebohangsopazi3@gmail.com>
2024-11-15 13:35:12 +0200

    Implement controller logic

--- app/Http/Controllers/BackgroundJobController.php
+++ app/Http/Controllers/BackgroundJobController.php
@@ -7,4 +7,19 @@ use Illuminate\Http\Request;
 class BackgroundJobController extends Controller
 {
     //
+    public function index(){
+
+        $jobs = [['id'=> 1, 'class' => 'Examplejob', 'method'=> 'execute', 'status'=>'running', 'retries'=>0],
+                ['id'=> 2, 'class' => 'Examplejob', 'method'=> 'execute', 'status'=>'failed', 'retries'=>2]
+    ];
+
+        return view('jobs.index', compact('jobs'));
+
+    }
+
+    public function cancel($id){
+        Log::info("Job with Id {$id} has been cancelled.");
+
+        return redirect()->route('jobs.index')->with('success', "job {$id} canceled.");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controller\BackgroundJobController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', [BackgroundJobController::class, 'index'])-> name('jobs.index');
+Route::post('/jobs/cancel/{id}', [BackgroundJobController::class, 'cancel'])-> name('jobs.cancel');
+
